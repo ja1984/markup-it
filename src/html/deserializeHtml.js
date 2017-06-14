ce0a244dbcc30c40201f1ad2a78b7a21ad0aea4e
Aaron O'Mullan <aaron.omullan@gmail.com>
2017-06-15 00:36:25 +0200

    Fix: sanitize non-breaking spaces (#75)
    * Add tests for non-breaking spaces
    
    * Sanitize and clean non-breaking spaces when parsing from HTML
    
    * Fix HTML tests which had non-breaking spaces in their output.yaml

--- src/html/deserializeHtml.js
+++ src/html/deserializeHtml.js
@@ -296,7 +296,8 @@ function parse(str) {
         },
 
         ontext(text) {
-            const textNode = Text.createFromString(text, marks);
+            const cleanText = sanitizeSpaces(text);
+            const textNode = Text.createFromString(cleanText, marks);
             appendNode(textNode);
         },
 
@@ -345,6 +346,17 @@ function parse(str) {
 
     const rootNodes = stack.peek().nodes;
     return List(rootNodes);
+}
+
+/*
+ * sanitizeSpaces replace non-breaking spaces with regular space
+ * non-breaking spaces (aka &nbsp;) are sources of many problems & quirks
+ * &nbsp; in ascii is `0xA0` or `0xC2A0` in utf8
+ * @param {String} str
+ * @return {String}
+ */
+function sanitizeSpaces(str) {
+    return str.replace(/\xa0/g, ' ');
 }
 
 /**
