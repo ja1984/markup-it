892f00359d96a6c6ff7183396c53f99dba892165
Gabin <hello@gabinaureche.com>
2019-03-20 16:46:21 +0100

    Add failing test

---
+++ test/from-markdown/html/markdown/input.md
@@ -0,0 +1,3 @@
+<div style="text-align: justify">
+This div contains Markdown with a [link](https://www.google.com) and some **bold content**.
+</div>
---
+++ test/from-markdown/html/markdown/output.js
@@ -0,0 +1,12 @@
+/** @jsx h */
+import h from 'h';
+
+export default (
+    <document>
+        <paragraph>
+            This div contains Markdown with a{' '}
+            <link href="https://www.google.com">link</link> and some{' '}
+            <bold>bold content</bold>.
+        </paragraph>
+    </document>
+);
