7117c8ea344dbdbd0825b542e9e9a7a366813d88
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-01-23 13:59:40 +0100

    Update Babel present-env targeted browsers list

--- babel.config.js
+++ babel.config.js
@@ -5,8 +5,25 @@ module.exports = {
             '@babel/preset-env',
             {
                 targets: {
-                    node: 'current'
-                }
+                    // Only browsers with support of async/await are supported
+                    // List based on the GitHub preset: https://github.com/github/babel-preset-github/blob/master/index.js
+                    browsers: [
+                        'last 8 Chrome versions',
+                        'last 4 Firefox versions',
+                        'last 3 Safari versions',
+                        'last 4 Edge versions',
+                        'Firefox ESR',
+                        'Chrome >= 55',
+                        'Firefox >= 63',
+                        'Safari >= 10.3',
+                        'Edge >= 15',
+                        'Opera >= 42'
+                    ]
+                },
+                loose: true,
+                modules: false,
+                useBuiltIns: 'usage',
+                corejs: 3
             }
         ],
         '@babel/preset-react'
